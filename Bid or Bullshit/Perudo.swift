caf22a2e4652190d77f91379687f23628077c1e8
maarten <maarten@experimenters-MacBook-Pro.local>
2017-03-04 16:47:07 +0100

    Added basic menu

---
+++ Bid or Bullshit/Perudo.swift
@@ -0,0 +1,13 @@
+//
+//  Perudo.swift
+//  Bid or Bullshit
+//
+//  Created by maarten on 04/03/2017.
+//  Copyright © 2017 M.A. van der Velde. All rights reserved.
+//
+
+import Foundation
+
+class Perudo {
+    
+}
